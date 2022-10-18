597860382f6798c911b3ef80648bb0d5f2c5c9bc
Geoffrey Morizot <47354322+GeoffreyMorizot@users.noreply.github.com>
2022-10-18 11:41:33 +0200

    feat:add file entity (#23)

--- src/middelware/middelware.ts
+++ src/middelware/middelware.ts
@@ -1,19 +1,32 @@
+import { Prisma } from "@prisma/client";
 import { IContext } from "../context";
 const bcrypt = require("bcrypt");
 
 
 export class Middelware {
 
-  static encryptPassword(context:IContext ) {
-    context.prisma.$use(async (params:any, next:any) => {
+  //Intercepte les requêtes de création de user et hash le password
+ /*  static encryptPassword(context: ()=>({ req }: any) => IContext) {
+    context.prisma.$use(async (params, next) => {
       if (params.action === "create" && params.model === "User") {
-        const user = params.args.data
-        const salt = bcrypt.genSaltSync(10)
-        const hash = bcrypt.hashSync(user.password, salt)
-        user.password = hash
+        try {
+          const user = params.args.data;
+          const salt = bcrypt.genSaltSync(10);
+          const hash = bcrypt.hashSync(user.password, salt);
+          user.password = hash;
+          return next(params);
+        } catch (error) {
+          console.log(error);
+        }
       }
-      return next(params);
     });
+  } */
+ 
+  static async excludePasswordMiddleware(params:Prisma.MiddlewareParams, next: (params: Prisma.MiddlewareParams) => Promise<any>){
+    const result = await next(params)
+    if (params?.model === 'User' && params?.args?.select?.password !== true) {
+      delete result.password
+    }
+    return result
   }
-    
 }

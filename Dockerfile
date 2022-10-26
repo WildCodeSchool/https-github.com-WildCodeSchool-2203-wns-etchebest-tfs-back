64af1807189bb42f2e701bae72938e434ae58a15
Geoffrey Morizot <47354322+GeoffreyMorizot@users.noreply.github.com>
2022-10-26 17:31:40 +0200

    feat: new seed with faker (#24)

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY src src
 COPY tsconfig.json tsconfig.json
 COPY prisma prisma
 ENV DATABASE_URL="mysql://root:r00t_TFs@mysql:3306/my_tfs_db"
-CMD npx prisma generate && npx prisma db push --force-reset && npm run start && npx prisma studio
+CMD npx prisma generate && npx prisma db push --force-reset && npx prisma db seed && npm run start && npx prisma studio
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,58 @@
-import { projects } from './data/projects'
-import { users } from './data/users'
-import { PrismaClient } from '@prisma/client'
+import { users } from "./data/users";
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import {
+  ProjectCreateInput,
+  ProjectCreateManyInput,
+  UserCreateInput,
+} from "./generated/type-graphql";
 
 const prisma = new PrismaClient();
 
-async function main(){
-/*   for (let project of projects){
-    await prisma.project.create({
-      data: project
-    })
-  } */
-  for (let user of users){
-    await prisma.user.create({
-      data: user
-    })
+
+async function main() {
+  const amountOfUsers = 50;
+  const amountOfProjects = 8;
+
+  const users: UserCreateInput[] = [];
+  const projects: ProjectCreateManyInput[] = [];
+
+  for (let index = 0; index < amountOfUsers; index++) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+
+    const user: UserCreateInput = {
+      firstname: firstName,
+      lastname: lastName,
+      email: faker.internet.email(firstName, lastName),
+      password: faker.internet.password(),
+      roles: faker.helpers.arrayElement(["USER", "ADMIN"]),
+    };
+
+    users.push(user);
   }
+
+/*   for (let index = 0; index < amountOfProjects; index++) {
+    const project: ProjectCreateManyInput = {
+      code: faker.lorem.word(3),
+      subject: faker.lorem.sentence(),
+      title: faker.lorem.sentence(),
+      user_author_project_id: 1
+    };
+    projects.push(project);
+  } */
+
+  const addUsers = async () => await prisma.user.createMany({ data: users });
+  addUsers();
+/*   const addProjects = async () => await prisma.project.createMany({ data: projects });
+  addProjects(); */
 }
 
-main().catch(e=>{
-  console.log(e);
-  process.exit(1);
-}).finally(()=>{
-  prisma.$disconnect();
-})
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });

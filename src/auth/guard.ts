c0a4238845d9ab3f1a7234a8c305d0176f485ada
Nicolas Theye <80323115+Nico786@users.noreply.github.com>
2023-01-05 17:59:03 +0100

    rename role user => dev (#39)

--- src/auth/guard.ts
+++ src/auth/guard.ts
@@ -5,20 +5,20 @@ import { Role } from "../../prisma/generated/type-graphql/enums/Role";
 
 export const resolversEnhanceMap: ResolversEnhanceMap = {
   User: {
-    user: [Authorized(Role.USER, Role.ADMIN)],
-    users: [Authorized(Role.ADMIN, Role.USER)],
-    updateUser: [Authorized(Role.INTERN, Role.USER, Role.LEAD, Role.ADMIN)],
-    deleteUser: [Authorized(Role.INTERN, Role.USER, Role.LEAD, Role.ADMIN)],
+    user: [Authorized(Role.DEV, Role.ADMIN)],
+    users: [Authorized(Role.ADMIN, Role.DEV)],
+    updateUser: [Authorized(Role.INTERN, Role.DEV, Role.LEAD, Role.ADMIN)],
+    deleteUser: [Authorized(Role.INTERN, Role.DEV, Role.LEAD, Role.ADMIN)],
   },
   Project: {
-    project: [Authorized(Role.INTERN, Role.USER, Role.LEAD, Role.ADMIN)],
-    projects: [Authorized(Role.INTERN, Role.USER, Role.LEAD, Role.ADMIN)],
+    project: [Authorized(Role.INTERN, Role.DEV, Role.LEAD, Role.ADMIN)],
+    projects: [Authorized(Role.INTERN, Role.DEV, Role.LEAD, Role.ADMIN)],
     createProject: [Authorized(Role.LEAD, Role.ADMIN)],
-    updateProject: [Authorized(Role.INTERN, Role.USER, Role.LEAD, Role.ADMIN)],
+    updateProject: [Authorized(Role.INTERN, Role.DEV, Role.LEAD, Role.ADMIN)],
     deleteProject: [Authorized(Role.LEAD, Role.ADMIN)],
   },
   Ticket: {
-    tickets: [Authorized(Role.INTERN, Role.USER, Role.LEAD, Role.ADMIN)],
-    ticket: [Authorized(Role.INTERN, Role.USER, Role.LEAD, Role.ADMIN)],
+    tickets: [Authorized(Role.INTERN, Role.DEV, Role.LEAD, Role.ADMIN)],
+    ticket: [Authorized(Role.INTERN, Role.DEV, Role.LEAD, Role.ADMIN)],
   },
 };
